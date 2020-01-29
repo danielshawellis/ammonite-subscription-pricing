1c6c121fdfa95a4680ab6fe58d51f9d6bdd5a00f
Daniel Ellis <danielshawellis@gmail.com>
2020-01-29 13:08:02 -0600

    add plugin headers

---
+++ ammonite-subscription-pricing.php
@@ -0,0 +1,8 @@
+<?php
+/**
+ * Plugin Name:       Ammonite Subscription Pricing
+ * Description:       Edits the single purchase price, allowing for greater flexibility with All Products for WooCommerce Subscriptions
+ * Version:           1.0.0
+ * Author:            Daniel Ellis
+ * Author URI:        https://danielellisdevelopment.com/
+ */
