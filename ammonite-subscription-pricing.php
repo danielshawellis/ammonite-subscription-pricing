6d79a81a5f3dd3fe68da436ea66c62adb8aa87ce
Daniel Ellis <danielshawellis@gmail.com>
2020-01-29 14:52:13 -0600

    set up basic dynamic pricing filter

--- ammonite-subscription-pricing.php
+++ ammonite-subscription-pricing.php
@@ -13,3 +13,25 @@
 if ( ! defined( 'ABSPATH' ) ) {
   die;
 }
+
+/*
+  Filter To Edit Prices for Single Purchases
+*/
+add_filter( 'woocommerce_dynamic_pricing_process_product_discounts', 'amdv_remove_dynamic_pricing_settings_from_one_time_option', 10, 4 );
+function amdv_remove_dynamic_pricing_settings_from_one_time_option( $eligible, $product, $discounter_name, $discounter_object ) {
+
+  // Log product to JS console for dev
+  $output = $product;
+  if (is_array($output))
+      $output = implode(',', $output);
+
+  echo "<script>console.log(" . $output . ");</script>";
+
+  // Check if product is one time option
+  // if (condition) {
+  //   $eligible = 0;
+  // }
+
+  return $eligible;
+
+}
