bdf1a0502dbef175b9a2dd7e2a8b06308197e06c
Daniel Ellis <danielshawellis@gmail.com>
2020-01-30 09:14:48 -0600

    rework away from dynamic pricing plugin using conditional repricing

--- ammonite-subscription-pricing.php
+++ ammonite-subscription-pricing.php
@@ -15,23 +15,36 @@ if ( ! defined( 'ABSPATH' ) ) {
 }
 
 /*
-  Filter To Edit Prices for Single Purchases
+  Edit Prices for Single Purchases VS Subscriptions
 */
-add_filter( 'woocommerce_dynamic_pricing_process_product_discounts', 'amdv_remove_dynamic_pricing_settings_from_one_time_option', 10, 4 );
-function amdv_remove_dynamic_pricing_settings_from_one_time_option( $eligible, $product, $discounter_name, $discounter_object ) {
+add_action( 'woocommerce_before_calculate_totals', 'add_custom_price', 20, 1);
+function add_custom_price( $cart ) {
 
-  // Log product to JS console for dev
-  $output = $product;
-  if (is_array($output))
-      $output = implode(',', $output);
+    // This is necessary for WC 3.0+
+    if ( is_admin() && ! defined( 'DOING_AJAX' ) )
+        return;
 
-  echo "<script>console.log(" . $output . ");</script>";
+    // Avoiding hook repetition (when using price calculations for example)
+    if ( did_action( 'woocommerce_before_calculate_totals' ) >= 2 )
+        return;
 
-  // Check if product is one time option
-  // if (condition) {
-  //   $eligible = 0;
-  // }
+    // Loop through cart items
+    foreach ( $cart->get_cart() as $item ) {
 
-  return $eligible;
+      // Check if a subscription option is selected for the item
+      if ( $item['wcsatt_data']['active_subscription_scheme'] != false ) {
 
+        // If a subscription option is selected, apply quantity-based discounts
+        if ( $item['quantity'] == 1 ) {
+          $item['data']->set_price( 45 );
+        } elseif ( $item['quantity'] == 2 ) {
+          $item['data']->set_price( 42.5 );
+        } elseif ( $item['quantity'] == 3 ) {
+          $item['data']->set_price( 41.66 );
+        } elseif ( $item['quantity'] >= 4 ) {
+          $item['data']->set_price( 41.25 );
+        }
+
+      }
+    }
 }
